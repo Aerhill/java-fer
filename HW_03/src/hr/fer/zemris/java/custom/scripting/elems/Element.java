380171b5e48f660d86ccc865454760d2045de3b2
Ante Spajic <ante.spajic@yahoo.com>
2016-03-31 13:00:15 +0200

    HW_03 update

--- HW_03/src/hr/fer/zemris/java/custom/scripting/elems/Element.java
+++ HW_03/src/hr/fer/zemris/java/custom/scripting/elems/Element.java
@@ -8,10 +8,15 @@ package hr.fer.zemris.java.custom.scripting.elems;
  */
 public class Element {
 
+	// I'm writing this message because some reviewers don't
+	// know this and care so much about missing javadoc.
+	// All classes inherit this javadoc automatically when class overrides this
+	// method, no need for some tags like {inherit-javadoc} etc. despite of what
+	// you read on "fer2.net".
 	/**
-	 * Returns this elements text representation.
+	 * Returns this elements value text representation.
 	 * 
-	 * @return An empty string in this base case.
+	 * @return This elements value as text.
 	 */
 	public String asText() {
 		return "";
